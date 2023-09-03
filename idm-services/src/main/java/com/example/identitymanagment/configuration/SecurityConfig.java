81f951ef1a1999114712e17c9cf793224351615d
“arslan.kucukkafa” <aslhckk@gmail.com>
2023-09-03 18:20:53 +0300

    * csrf is disable and added swagger in pom.xml

--- idm-services/src/main/java/com/example/identitymanagment/configuration/SecurityConfig.java
+++ idm-services/src/main/java/com/example/identitymanagment/configuration/SecurityConfig.java
@@ -3,14 +3,44 @@ package com.example.identitymanagment.configuration;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
+
 @Configuration
+@EnableWebSecurity
 public class SecurityConfig {
+    private static final String[] LogRegisterControllerEndpoints = {
+            "/v2/api-docs",
+            "/swagger-resources",
+            "/swagger-resources/**",
+            "/swagger-ui.html",
+            "/v3/api-docs/**",
+            "/swagger-ui/**",
+            "/api/v1/laboratories/save",
+            "/api/v1/laboratories/login",
+            "/idm/sign-up",
+            "/idm/sign-in",
+
+    };
+
     @Bean
-    SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-        http.authorizeHttpRequests().anyRequest().permitAll();
-        return http.build();
+    public PasswordEncoder encoder() {
+        return new BCryptPasswordEncoder();
     }
 
+    @Bean
+    SecurityFilterChain web(HttpSecurity http) throws Exception {
+        http
+                // ...
+                .authorizeHttpRequests(authorize -> authorize
+                        .requestMatchers("/**").permitAll()
+                        .requestMatchers("/admin/**").hasRole("ADMIN")
+                        .anyRequest().denyAll()
+                );
+        http.csrf().disable();
+        return http.build();
+    }
 }
